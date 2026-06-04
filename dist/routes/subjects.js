bad6a422a64ff63aa42aa98f37511030893b64db
MightyBeast <variola@my.centennialcollege.ca>
2026-06-04 13:50:09 -0400

    updated cors origin

--- dist/routes/subjects.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getAllSubjects } from "../controllers/subjectsContoller";
-const router = express.Router();
-//Get All Subjects
-router.get("/", getAllSubjects);
-export default router;
-//# sourceMappingURL=subjects.js.map
--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ const app = express();
 const PORT = 5001;
 
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: [process.env.FRONTEND_URL || 'http://localhost:5173', 'https://class-mgmt-frontend-v1.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
