b94ba2d237ded2306941faff4a67c07f601c68ec
MightyBeast <variola@my.centennialcollege.ca>
2026-07-03 14:55:03 -0400

    update cors

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,7 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://class-mgmt-frontend-v1.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
